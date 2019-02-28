2bb4a517af394dac1d35bc04694e0ff79573d856
SamPerCap <31726611+SamPerCap@users.noreply.github.com>
2019-02-28 11:15:38 +0100

    Kris' code refactor

--- app/src/main/java/com/example/rolldicecup/MainActivity.java
+++ app/src/main/java/com/example/rolldicecup/MainActivity.java
@@ -10,9 +10,10 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    String TAG = "Logs";
+    String TAG = "Developer";
+    ImageView Iv1, Iv2, Iv3, Iv4, Iv5, Iv6;
 
-    int[] images = {  R.drawable.dice_side_1
+    int[] images = {R.drawable.dice_side_1
             , R.drawable.dice_side_2
             , R.drawable.dice_side_3
             , R.drawable.dice_side_4
@@ -20,53 +21,37 @@ public class MainActivity extends AppCompatActivity {
             , R.drawable.dice_side_6};
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.e(TAG, "It boots up");
+        Log.d(TAG, "It boots up");
+        LocateItems();
     }
 
-    public void rollDice(View view)
-    {
-        int dice1 = randomNumber(0,5);
-        int dice2 = randomNumber(0,5);
-        int dice3 = randomNumber(0,5);
-        int dice4 = randomNumber(0,5);
-        int dice5 = randomNumber(0,5);
-        int dice6 = randomNumber(0,5);
-
-        Log.e(TAG, "I Hit the rollDice()");
-
-        ImageView imgLayout1 = findViewById(R.id.imageView);
-        ImageView imgLayout2 = findViewById(R.id.imageView2);
-
-        Log.e(TAG, "Gets the images");
-
-        int n = new Random().nextInt(1);
-
-        Log.e(TAG, "Random Numbers calculator");
-
-        ImageView fl = findViewById(R.id.imageView);
-        ImageView fl2 = findViewById(R.id.imageView2);
-        ImageView fl3 = findViewById(R.id.imageView3);
-        ImageView fl4 = findViewById(R.id.imageView4);
-        ImageView fl5 = findViewById(R.id.imageView5);
-        ImageView fl6 = findViewById(R.id.imageView6);
-        fl.setImageResource(images[dice1]);
-        fl2.setImageResource(images[dice2]);
-        fl3.setImageResource(images[dice3]);
-        fl4.setImageResource(images[dice4]);
-        fl5.setImageResource(images[dice5]);
-        fl6.setImageResource(images[dice6]);
-
-        Log.e(TAG, "image array and then adding images");
+    public void rollDice(View view) {
+        Log.d(TAG, "I Hit the rollDice() and random number is generating. Also set image");
+        Iv1.setImageResource(images[randomNumber()]);
+        Iv2.setImageResource(images[randomNumber()]);
+        Iv3.setImageResource(images[randomNumber()]);
+        Iv4.setImageResource(images[randomNumber()]);
+        Iv5.setImageResource(images[randomNumber()]);
+        Iv6.setImageResource(images[randomNumber()]);
 
     }
 
-    private int randomNumber(int min, int max)
-    {
+    private void LocateItems() {
+        Log.d(TAG, "Locate items");
+        Iv1 = findViewById(R.id.imageView);
+        Iv2 = findViewById(R.id.imageView2);
+        Iv3 = findViewById(R.id.imageView3);
+        Iv4 = findViewById(R.id.imageView4);
+        Iv5 = findViewById(R.id.imageView5);
+        Iv6 = findViewById(R.id.imageView6);
+
+    }
+
+    private int randomNumber() {
         Random random = new Random();
-        return random.nextInt(max - min + 1)+min;
+        return random.nextInt(6);
     }
 }
