b4c570594a6fbaa9c8d85848e34914adcf698caa
liverpoljer <liverpoljer@live.dk>
2019-03-01 12:25:24 +0100

    singleton

--- app/src/main/java/com/example/rolldicecup/Model/ModelHistory.java
+++ app/src/main/java/com/example/rolldicecup/Model/ModelHistory.java
@@ -6,8 +6,18 @@ import java.util.ArrayList;
 
 public class ModelHistory {
     private ArrayList<History> historyList = new ArrayList<>();
-    public ModelHistory() {
+    private static volatile ModelHistory instance;
 
+
+    public static ModelHistory getInstance() {
+        if (instance == null) {
+            synchronized (ModelHistory.class) {
+                if (instance == null) {
+                    instance = new ModelHistory();
+                }
+            }
+        }
+        return instance;
     }
     public void AddHistory(History history)
     {
